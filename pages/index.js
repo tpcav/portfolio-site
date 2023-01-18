0b3383da1a7799ccc78187b5ba8e8a2466028b05
c <thomascavalcante77@gmail.com>
2023-01-18 13:16:40 -0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -125,8 +125,8 @@ export default function Example() {
           </div>  
 
           <div className="block m-4">
-            <h1 className="mt-2 flex text-7xl font-black tracking-tighter text-gray-200">TOM CAV</h1>
-            <div class="mt-4 flex space-x-2 text-gray-700">
+            <h1 className="mt-2 flex text-7xl font-black tracking-tighter text-gray-200">Tom Cav</h1>
+            <div class="mt-4 ml-1 flex space-x-2 text-gray-700">
               <p className="text-gray-600">Social Links: </p>
               <Link href="http://youtube.com/">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 488 512" class="w-5 h-5">! Font Awesome Pro 6.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fontico<path fill="currentColor" d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"/></svg>
