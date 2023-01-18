a4926002b0ff6d4a8ef188b0d6159d8892bf07bc
c <thomascavalcante77@gmail.com>
2023-01-18 13:56:53 -0500

    added meta tags

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Link from "next/link"
 import Image from "next/image"
+import Head from 'next/head'
+
 
 const posts = [
   {
@@ -115,6 +117,12 @@ const posts = [
 export default function Example() {
   return (
     <div className="relative bg-zinc-900 px-4 pb-20 lg:px-8 lg:pt-10 lg:pb-28">
+      <Head>
+        <title>Tom Cav. 2023</title>
+        <meta name="description" content="Developer, Designer, Photographer"/>
+        <meta name="keywords" content="Thomas Cavalcante, Tom Cav, Cavalcante, web development, product design, start-ups, student, Drexel University, Drexel"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <div className="absolute inset-0">
       </div>
       <div className="relative mx-auto max-w-7xl">
